789b67f13870b85ff373a1e8f43bbeb0d6097bbd
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-05 16:21:40 -0500

    Update centos7_node.pp

--- site/profile/manifests/centos7_node.pp
+++ site/profile/manifests/centos7_node.pp
@@ -1,15 +1,8 @@
 class profile::centos7_node {
   #  Installing Collector Package
-  package { "collector":
-    ensure=> "present",
-   }
-    
-   #Installation file
-   file {"files/collectors/linux/vityl-collector-2.11.0-1.el7.x86_64.rpm":
-     ensure=> "present",
-     }
-    
+  package{'vityl-collector-2.11.0-1.el7.x86_64.rpm':
+            provider => 'rpm',
+            ensure => 'present',
+            source => 'puppet:///files/collector/linux/vityl-collector-2.11.0-1.el7.x86_64.rpm',
+         }
 }
-
-# Class Declaration
-include profile::centos7_node
