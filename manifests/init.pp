a1f8c4baab16b1a89610729874494b8d2791ccbc
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-07 15:29:45 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,18 @@
 class collector_package {$source, $ensure='installed') {
   file {'/files/vityl-collector-2.11.0-1.el7.x86_64.rpm': source => $source }
-  package { $name:
+    package { 'vityl-collector-2.11.0-1.el7.x86_64.rpm':
     ensure => $ensure,
     provider => 'rpm',
+    source => '/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
+    required => File['/files/vityl-collector-2.11.0-1.el7.x86_64.rpm'],
+    }
+  }
+  file {'/files/vityl-collector-2.11.0.exe': source => $source }
+    package { 'vityl-collector-2.11.0.exe':
+    ensure => installed,
+    provider => 'rpm',
     source => '/files/$name.rpm',
-    required => File['/files/$name.rpm']
+    required => File['/files/vityl-collector-2.11.0.exe'],
+#    install_options => ['/VERYSILENT','','']
   }
 }
