54f5643256869086621a2288936e119bd91c87de
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-07 18:15:10 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,16 +3,15 @@ class collector_package {$source, $ensure='installed') {
     package { 'vityl-collector-2.11.0-1.el7.x86_64.rpm':
     ensure => $ensure,
     provider => 'rpm',
-    source => '/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
-    required => File['/files/vityl-collector-2.11.0-1.el7.x86_64.rpm'],
+    source => 'puppet:///modules/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
+    required => File['puppet:///modules/files/vityl-collector-2.11.0-1.el7.x86_64.rpm'],
     }
   }
-  file {'/files/vityl-collector-2.11.0.exe': source => $source }
+  file {'puppet:///modules/files/vityl-collector-2.11.0.exe': source => $source }
     package { 'vityl-collector-2.11.0.exe':
-    ensure => installed,
-    provider => 'rpm',
-    source => '/files/$name.rpm',
-    required => File['/files/vityl-collector-2.11.0.exe'],
+    ensure => $ensure,
+    source => 'puppet:///modules/files/$name.rpm',
+    required => File['puppet:///modules/files/vityl-collector-2.11.0.exe'],
 #    install_options => ['/VERYSILENT','','']
   }
 }
