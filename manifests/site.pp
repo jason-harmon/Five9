37933ffa0225d2f07208353f5a72dbb5b108ebea
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-09 17:03:57 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,6 +10,12 @@ class vityl-collectorinstall {
       source => '/etc/puppetlabs/code/environments/production/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
       install_options => ['--install'],
     }
+    
+    file { '/etc/puppetlabs/code/environments/production/files/guids/' + $host + '.guid.properites':
+       source => '/etc/vityl/collector/guid.properties',
+       replace => true,
+    
+  }
   
   if $osfamily == 'Windows' {
     package { 'vityl-collector-windows':
@@ -20,4 +26,8 @@ class vityl-collectorinstall {
 
     exec { 'sc config vitylcollector start= auto'
   }
+  
+    file { 'C:\\puppetlabs\\code\\environments\\production\\files\\guids\\' + $host + '.guid.properites':
+       source => '/etc/vityl/collector/guid.properties',
+       replace => true,
 }
