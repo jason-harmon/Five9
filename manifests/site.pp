a53452ad9f2c67517dc625d8ce3d51c9478ee6a6
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-14 16:06:45 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,14 +7,14 @@ class vitylcollectorInstall {
 #    mode => '0644',
       provider => 'rpm',
       ensure => installed,
-      source => 'puppet:///puppet/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
+      source => '/etc/puppetlabs/code/environments/production/modules/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
       install_options => ['--install'],
     }
     
     file { 'linuxguidreplace':
 	   # Ensure that the guid file exists on the server, if it does then replace guid on client
-       ensure => 'puppet:///puppet/files/guids/' + $host + '.guid.properites', 
-	     source => 'puppet:///puppet/files/guids/' + $host + '.guid.properites', 
+       ensure => '/etc/puppetlabs/code/environments/production/modules/files/guids/' + $host + '.guid.properites', 
+	     source => '/etc/puppetlabs/code/environments/production/modules/files/guids/' + $host + '.guid.properites', 
        path => '/etc/vityl/collector/guid.properties',
 	   replace => true,
     }
@@ -23,7 +23,7 @@ class vitylcollectorInstall {
   if $osfamily == 'Windows' {
     package { 'vityl-collector-windows':
       ensure => installed,
-      source => 'puppet:///files/vityl-collector-2.11.0.exe',
+      source => '/etc/puppetlabs/code/environments/production/modules/files/vityl-collector-2.11.0.exe',
       install_options => ['/exenoui /qn /l*v install.log'],
     }
 
@@ -33,8 +33,8 @@ class vitylcollectorInstall {
     
     file { 'winguidreplace':
 	   # Ensure that the guid file exists on the server, if it does then replace guid on client
-       ensure => 'puppet:///files/guids/' + $host + '.guid.properites',
-	     source => 'puppet:///files/guids/' + $host + '.guid.properites',
+       ensure => '/etc/puppetlabs/code/environments/production/modules/files/guids/' + $host + '.guid.properites',
+	     source => '/etc/puppetlabs/code/environments/production/modules/files/guids/' + $host + '.guid.properites',
 	     path => 'C:/ProgramData/Vityl/Collector/guid.properites',
        replace => true,
     }
