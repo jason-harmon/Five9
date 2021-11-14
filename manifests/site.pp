300f8bf6e9a634fe6f872ad377de6013aee74738
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-14 16:49:45 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,7 +7,7 @@ class vitylcollectorInstall {
 #    mode => '0644',
       provider => 'rpm',
       ensure => present,
-      source => '/etc/puppetlabs/code/environments/production/modules/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
+      source => '/etc/puppet/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
       install_options => ['--install'],
     }
     
