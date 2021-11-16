364560664a257751c2ade3b74afe53606cb63950
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-16 10:49:09 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,29 +1,5 @@
-class vitylcollectorInstall {
+class vitylcollectorUpdate {
 
-  if $osfamily == 'CentOS' {
-    package { 'vityl-collector-linux':
-#    owner => 'Administrator',
-#    group => 'Users',
-#    mode => '0644',
-      provider => 'rpm',
-      ensure => installed,
-      source => '/etc/puppetlabs/code/environments/production/modules/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
-      install_options => ['--install'],
-    } # package { 'vityl-collector-linux':
-   
-   
-  } #   if $osfamily == 'CentOS' 
-  
-  if $osfamily == 'Windows' {
-    package { 'vityl-collector-windows':
-      ensure => installed,
-      source => '/etc/puppetlabs/code/environments/production/modules/files/vityl-collector-2.11.0.exe',
-      install_options => ['/exenoui /qn /l*v install.log'],
-    }  # package { 'vityl-collector-windows':
 
-    exec { 'start_service' : 
-       command => 'sc config vitylcollector start= auto', 
-    }  # exec
-    
-  }  # if $osfamily == 'Windows' 
+
 }
