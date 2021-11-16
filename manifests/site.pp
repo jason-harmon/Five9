d877384407b20ff8f2f782a804d21fd4c8266b38
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-16 10:47:52 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,34 +9,21 @@ class vitylcollectorInstall {
       ensure => installed,
       source => '/etc/puppetlabs/code/environments/production/modules/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
       install_options => ['--install'],
-    }
-    
-    file { 'linuxguidreplace':
-	   # Ensure that the guid file exists on the server, if it does then replace guid on client
-       ensure => '/etc/puppetlabs/code/environments/production/modules/files/guids/' + $host + '.guid.properites', 
-	     source => '/etc/puppetlabs/code/environments/production/modules/files/guids/' + $host + '.guid.properites', 
-       path => '/etc/vityl/collector/guid.properties',
-	   replace => true,
-    }
-  }
+    } # package { 'vityl-collector-linux':
+   
+   
+  } #   if $osfamily == 'CentOS' 
   
   if $osfamily == 'Windows' {
     package { 'vityl-collector-windows':
       ensure => installed,
       source => '/etc/puppetlabs/code/environments/production/modules/files/vityl-collector-2.11.0.exe',
       install_options => ['/exenoui /qn /l*v install.log'],
-    }
+    }  # package { 'vityl-collector-windows':
 
     exec { 'start_service' : 
        command => 'sc config vitylcollector start= auto', 
-    }
+    }  # exec
     
-    file { 'winguidreplace':
-	   # Ensure that the guid file exists on the server, if it does then replace guid on client
-       ensure => '/etc/puppetlabs/code/environments/production/modules/files/guids/' + $host + '.guid.properites',
-	     source => '/etc/puppetlabs/code/environments/production/modules/files/guids/' + $host + '.guid.properites',
-	     path => 'C:/ProgramData/Vityl/Collector/guid.properites',
-       replace => true,
-    }
-  }
+  }  # if $osfamily == 'Windows' 
 }
