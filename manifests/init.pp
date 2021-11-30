215435e4dd0a8c7b26a372192b70cac3d31d7482
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-11-30 15:24:04 -0600

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@ class vitylcollectorInstall {
 #    mode => '0644',
       provider => 'rpm',
       ensure => installed,
-      source => '/etc/puppetlabs/code/environments/production/files/vityl-collector-2.11.0-1.el7.x86_64.rpm',
+      source => '/opt/vityl/boot/vityl-collector-2.11.0-1.el7.x86_64.rpm',
       install_options => ['--install'],
     } # package { 'vityl-collector-linux':
    
@@ -24,7 +24,7 @@ class vitylcollectorInstall {
   if $osfamily == 'Windows' {
     package { 'vityl-collector-windows':
       ensure => installed,
-      source => '/etc/puppetlabs/code/environments/production/files/vityl-collector-2.11.0.exe',
+      source => '/opt/vityl/boot/vityl-collector-2.11.0.exe',
       install_options => ['/exenoui /qn /l*v install.log'],
     }  # package { 'vityl-collector-windows':
 
