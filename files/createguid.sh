33535d4595a474de9734411e7dc35da58e58dac9
jason-harmon <49253129+jason-harmon@users.noreply.github.com>
2021-12-07 09:22:59 -0600

    Update createguid.sh

--- files/createguid.sh
+++ files/createguid.sh
@@ -6,7 +6,7 @@ cat >> /etc/systemd/system/vityl-collector.service.d/override.conf <<EOL
 [Service]
 User=root
 Group=root
-Environment=VCM_GUID_TYPE=$hostname
+Environment=VCM_GUID_TYPE=host 
 EOL
 
 chmod 644 /etc/systemd/system/vityl-collector.service.d/override.conf
